83cfb2fa4aabba76c03a9f3677e747d5e67d660e
Santi L <lucassantieng@gmail.com>
2019-06-19 09:15:58 -0300

    fundamentos - importação do react ola mundo

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//p1
+// const element = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDOM.render("Ola mundo", element);
+
+//p2
+const element = document.getElementById('root');
+
+ReactDOM.render(<h1>Ola Mundo</h1>, element);
